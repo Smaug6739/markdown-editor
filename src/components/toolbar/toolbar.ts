4ec385c85cf88c2f224e2b2696c3ef5dca67b401
Raphael <veauville.raphael@gmail.com>
2021-06-30 15:44:29 +0200

    Update

--- src/components/toolbar/toolbar.ts
+++ src/components/toolbar/toolbar.ts
@@ -24,60 +24,64 @@ export default defineComponent({
 			}
 			return null;
 		},
-		addAttributeSelection(attribute: string, start?: boolean, end?: boolean) {
+		addAttributeSelection(start: string | null, end: string | null) {
 			const startSelection = this.textarea!.selectionStart;
 			const endSelection = this.textarea!.selectionEnd;
 			const splitText = this.textarea!.value.split("");
 			splitText.forEach((c: string, i: number) => {
-				if (i === startSelection && start) splitText[i] = `${attribute}${c}`;
-				if (i === endSelection - 1 && end) splitText[i] = `${c}${attribute}`;
+				if (i === startSelection && start) splitText[i] = `${start}${c}`;
+				if (i === endSelection - 1 && end) splitText[i] = `${c}${end}`;
 			});
 			return splitText.join("");
 		},
-		addAttributePosition(attribute: string, start?: boolean, end?: boolean) {
+		addAttributePosition(start: string | null, end: string | null) {
 			const startSelection = this.textarea!.selectionStart;
 
-			if (!startSelection)
-				return (this.textarea!.value += `${start ? attribute : ""}${end ? attribute : ""
-					}`);
+			if (!startSelection) return (this.textarea!.value += `${start ? start : ""}${end ? end : ""}`);
 			const splitText = this.textarea!.value.split("");
-			if (this.textarea!.value.length === startSelection) return this.textarea!.value += `${attribute}${attribute}`
+			if (this.textarea!.value.length === startSelection) return this.textarea!.value += `${start}${end}`
 			splitText.forEach((c: string, i: number) => {
 				if (i === startSelection)
-					splitText[i] = `${start ? attribute : ""}${end ? attribute : ""}${c}`;
+					splitText[i] = `${start ? start : ""}${end ? end : ""}${c}`;
 			});
 			return splitText.join("");
 		},
-		checkAndChange(attribute: string, start: boolean, end: boolean) {
+		checkAndChange(start: string | null, end: string | null) {
 			if (this.getSelectionText()) {
 				this.$emit("CHANGE_TEXT", {
-					value: this.addAttributeSelection(attribute, start, end),
+					value: this.addAttributeSelection(start, end),
 				});
 			} else {
 				this.$emit("CHANGE_TEXT", {
-					value: this.addAttributePosition(attribute, start, end),
+					value: this.addAttributePosition(start, end),
 				});
 			}
 		},
 		markdown(type: string): void {
 			switch (type.toLowerCase()) {
 				case "bold":
-					this.checkAndChange("**", true, true)
+					this.checkAndChange("**", "**")
 					break;
 				case "italic":
-					this.checkAndChange("**", true, true)
-
+					this.checkAndChange("*", "*")
+					break;
+				case "strike":
+					this.checkAndChange("<s>", '</s>')
+					break;
+				case "underline":
+					this.checkAndChange("<u>", "</u>")
 					break;
 				case "h1":
-					if (this.getSelectionText()) {
-						this.$emit("CHANGE_TEXT", {
-							value: this.addAttributeSelection("# ", true, false),
-						});
-					} else {
-						this.$emit("CHANGE_TEXT", {
-							value: this.addAttributePosition("# ", true, false),
-						});
-					}
+					this.checkAndChange("# ", null)
+					break;
+				case "h2":
+					this.checkAndChange("## ", null)
+					break;
+				case "h3":
+					this.checkAndChange("### ", null)
+					break;
+				case "quote":
+					this.checkAndChange(">", null)
 					break;
 			}
 		},
